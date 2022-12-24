a4fe1a674450c1bc9cb620ede34c6080c2780e78
Jonas Silva <jonaslsa@uio.no>
2022-12-24 15:11:47 +0100

    Responsive flex

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,13 +10,14 @@ const Home: NextPage = () => {
         <meta name="description" content="Jonas Silva personal website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
-        <div className="container flex flex-row items-center justify-center gap-12 px-4 py-16 ">
+      <main className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 sm:flex-row">
          <div>
             <h1 className="text-4xl font-bold">jonaslsa</h1>
             <h2 className="text-sky-300">Jonas Lien Sampaio da Silva</h2>
          </div>
-         <div style={{ borderLeft: "1px solid white", height: "12rem" }}></div>
+         <div style={{ borderLeft: "1px solid white", height: "12rem" }} className="hidden sm:block opacity-60"></div>
+         <div style={{ borderTop: "1px solid white", width: "14rem" }} className="block sm:hidden opacity-60"></div>
          <div>
             <ProjectLink link="https://github.com/TheVoxcraft/" title="Github" />
             <ProjectLink link="https://www.linkedin.com/in/jonas-silva-b1a628ba/" title="Linkedin" />
