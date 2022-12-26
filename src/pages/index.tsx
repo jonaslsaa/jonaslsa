b6bbe3c9ea34f7d207007c5ece99ecd6642a25aa
Jonas Silva <jonaslsa@uio.no>
2022-12-26 16:32:54 +0100

    smal change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,9 @@ const Home: NextPage = () => {
          <div>
             <ProjectLink link="https://github.com/TheVoxcraft/" title="Github" />
             <ProjectLink link="https://www.linkedin.com/in/jonas-silva-b1a628ba/" title="Linkedin" />
+
             <hr className="border-gray-700 my-2" />
+            
             <ProjectLink link="http://tracker.jonaslsa.com" title="Tracker" />
             <ProjectLink link="s/" title="URL Shortener" />
             <ProjectLink link="https://www.multure.us/" title="Multure - AI managed CMS" />
