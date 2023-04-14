46163e7ea3dc32da94ebdbe2b7e242a90920091b
Jonas Silva <jonaslsa@uio.no>
2023-04-14 15:16:42 +0200

    Switch to OSM maps (dark mode)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Analytics } from '@vercel/analytics/react' ;
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import "../styles/leaflet.css"
 
 const MyApp: AppType = ({Component, pageProps}) => {
   return (
