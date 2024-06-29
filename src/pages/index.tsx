d6e64b693bf6e8d8cc07e44cc55beb96728eb3de
Jonas Silva <jonaslsa@uio.no>
2024-06-29 22:26:56 +0200

    Update homepage GitHub link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,20 +12,22 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 sm:flex-row">
-         <div>
+          <div>
             <h1 className="text-4xl font-bold">jonaslsa</h1>
             <h2 className="text-sky-300">Jonas Lien Sampaio da Silva</h2>
-         </div>
-         <div style={{ borderLeft: "1px solid white", height: "12rem" }} className="hidden sm:block opacity-60"></div>
-         <div style={{ borderTop: "1px solid white", width: "14rem" }} className="block sm:hidden opacity-60"></div>
-         <div>
+          </div>
+          <div style={{ borderLeft: "1px solid white", height: "12rem" }} className="hidden sm:block opacity-60"></div>
+          <div style={{ borderTop: "1px solid white", width: "14rem" }} className="block sm:hidden opacity-60"></div>
+          <div>
             <ProjectLink link="https://github.com/jonaslsaa/" title="Github" />
             <ProjectLink link="https://www.linkedin.com/in/jonas-silva-b1a628ba/" title="Linkedin" />
 
             <hr className="border-gray-700 my-2" />
             <ProjectLink link="s/" title="URL Shortener" />
             <ProjectLink link="paste/" title="Pastebin" />
-         </div>
+            <ProjectLink link="/blip/" title="Blip - Real-time incident mapping" />
+            <ProjectLink link="https://github.com/jonaslsaa/multure-cms/" title="Multure - AI managed News CMS" />
+          </div>
         </div>
       </main>
     </>
